69db7d8e2f3037b8c76fc96e8327a603209a61c2
Oleg Polyanskiy <ol.polyanskiy@gmail.com>
2025-05-19 10:13:55 +0300

    feat: add suspense to data-dependent components

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,12 +16,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -43,6 +43,9 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
@@ -72,6 +75,8 @@ export async function fetchCardData() {
     const numberOfCustomers = Number(data[1][0].count ?? '0');
     const totalPaidInvoices = formatCurrency(data[2][0].paid ?? '0');
     const totalPendingInvoices = formatCurrency(data[2][0].pending ?? '0');
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     return {
       numberOfCustomers,
